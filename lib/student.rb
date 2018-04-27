1527f14860eb8d78a02434ab3c1d0bbb58b3251b
David Ritchey <ritchey0713@gmail.com>
2018-04-27 01:55:27 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -22,8 +22,13 @@ class Student
   end 
   
   def self.drop_table 
-    sql = 
-      
+    sql = <<-SQL 
+      DROP TABLE IF EXISTS students
+      SQL
+    DB[:conn].execute(sql)
+  end
+  
+  
     
   
 
